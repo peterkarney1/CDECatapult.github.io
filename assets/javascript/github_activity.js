fdba3c2739813234a56e46e507f7a775cd5c0dbf
Tim Davies <tim@errorstudio.co.uk>
2014-01-22 17:21:59 +0000

    Improve Github Activity styling

--- assets/javascript/github_activity.js
+++ assets/javascript/github_activity.js
@@ -1,3 +1,12 @@
+Handlebars.registerHelper('replace', function(text, str_to_replace, str_to_replace_with) {
+  return text.replace(str_to_replace, str_to_replace_with);
+});
+
+Handlebars.registerHelper('truncate', function(text, num_chars) {
+  return text.slice(0, num_chars);
+});
+
+
 $(function() {
   if ($('#github-activity')[0]) {
     // Make request to Github:
