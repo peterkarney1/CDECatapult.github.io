daade6e6dab206c8ed9d62a04c85965b21455fee
Tim Davies <tim@errorstudio.co.uk>
2014-01-29 16:24:28 +0000

    Let the user change the choropleth colour scheme

--- howto/choropleth-maps/diabetes-per-head-per-ccg-per-month.js
+++ howto/choropleth-maps/diabetes-per-head-per-ccg-per-month.js
@@ -1,30 +1,61 @@
-var diabetes_per_head_per_ccg_per_month = function(div) {
-  var map = L.map(div).setView([53.0, -1.5], 6);
+window.colour_0 = '#333333';
+window.colour_1 = '#0C2C84';
+window.colour_2 = '#225EA8';
+window.colour_3 = '#1D91C0';
+window.colour_4 = '#41B6C4';
+window.colour_5 = '#7FCDBB';
+window.colour_6 = '#C7E9B4';
+window.colour_7 = '#FFFFCC';
+
+
+var DiabetesChoropleth = {
+  init: function(div) {
+    this.div = div;
+    this.map = L.map(this.div).setView([53.0, -1.5], 6);
+    
+    L.tileLayer('http://{s}.tile.cloudmade.com/{key}/22677/256/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2012 CloudMade',
+      key: 'BC9A493B41014CAABB98F0471D759707'
+    }).addTo(this.map);
+    
+    mergedFeatureLayer(this.map, "data/diabetes_per_head_per_ccg_per_month.csv", "data/ccg-boundaries.json", "ccg_code", this.style, null, null, "ccg_boundaries");
+    
+    addLegend([0, 20, 22, 24, 26, 28, 30, 34], this.map, this.color);
+  },
   
-  var color = function(d) {
-    return  d == 'NA' ? '#333' :
-            d == 'undefined' ? '#333' :
-            d > 30 ? '#0C2C84' :
-            d > 28 ? '#225EA8' :
-            d > 26 ? '#1D91C0' :
-            d > 24 ? '#41B6C4' :
-            d > 22 ? '#7FCDBB' :
-            d > 20 ? '#C7E9B4' :
-            '#FFFFCC';
-  };
+  destroy: function() {
+    this.map.remove();
+  },
   
-  var style = function(feature) {
+  refresh: function() {
+    this.destroy();
+    this.init(this.div);
+  },
+  
+  color: function(d) {
+    return  d == 'NA' ? window.colour_0 :
+            d == 'undefined' ? window.colour_0 :
+            d > 30 ? window.colour_1 :
+            d > 28 ? window.colour_2 :
+            d > 26 ? window.colour_3 :
+            d > 24 ? window.colour_4 :
+            d > 22 ? window.colour_5 :
+            d > 20 ? window.colour_6 :
+            window.colour_7;
+  },
+  
+  style: function(feature) {
     return {
-      fillColor: color(feature.properties.per_capita_spend),
+      fillColor: DiabetesChoropleth.color(feature.properties.per_capita_spend),
       weight: 2,
       opacity: 0.5,
       color: 'white',
       dashArray: '3',
       fillOpacity: 0.8
     }
-  };
+  },
   
-  var defaultStyle = function(feature) {
+  defaultStyle: function(feature) {
     return {
       outlineColor: "#000000",
       outlineWidth: 0.5,
@@ -32,14 +63,19 @@ var diabetes_per_head_per_ccg_per_month = function(div) {
       opacity: 1,
       fillOpacity: 0
     };
-  };
-  
-  L.tileLayer('http://{s}.tile.cloudmade.com/{key}/22677/256/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2012 CloudMade',
-    key: 'BC9A493B41014CAABB98F0471D759707'
-  }).addTo(map);
-  
-  mergedFeatureLayer(map, "data/diabetes_per_head_per_ccg_per_month.csv", "data/ccg-boundaries.json", "ccg_code", style, null, null, "ccg_boundaries");
-  
-  addLegend([0, 20, 22, 24, 26, 28, 30, 34], map, color);
+  }
 }
+
+$(function() {
+  $('button.colour-scheme-change').click(function() {
+    window.foo = $(this).closest('div.row');
+    window.colour_1 = $(this).closest('div.row').find('div.colour_1').css('background-color');
+    window.colour_2 = $(this).closest('div.row').find('div.colour_2').css('background-color');
+    window.colour_3 = $(this).closest('div.row').find('div.colour_3').css('background-color');
+    window.colour_4 = $(this).closest('div.row').find('div.colour_4').css('background-color');
+    window.colour_5 = $(this).closest('div.row').find('div.colour_5').css('background-color');
+    window.colour_6 = $(this).closest('div.row').find('div.colour_6').css('background-color');
+    window.colour_7 = $(this).closest('div.row').find('div.colour_7').css('background-color');
+    DiabetesChoropleth.refresh();
+  });
+});
